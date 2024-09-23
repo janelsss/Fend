1d17e12f53aaa5a79f0b080bc8a7010abebcf7fc
PransLoren <jaaq.borje.up@phinmaed.com>
2024-09-23 23:36:35 +0800

    mini

--- lib/model/student_model.dart
+++ lib/model/student_model.dart
@@ -19,10 +19,10 @@ class Student {
     return Student(
       id: json['id'] ?? 0,
       firstname: json['firstname'] ?? '',
-      lastname: json['lastname']?? '',
-      course: json['course']?? '',
-      year: json['year'] is String ? json['year'] : '',
-      enrolled: json['enrolled'] == 'true',
+      lastname: json['lastname'] ?? '',
+      course: json['course'] ?? '',
+      year: json['year'] != null ? json['year'].toString() : '', // Updated to handle null
+      enrolled: json['enrolled'] is bool ? json['enrolled'] : (json['enrolled'] == 'true'),
     );
   }
 
@@ -33,7 +33,7 @@ class Student {
       'lastname': lastname,
       'course': course,
       'year': year,
-      'enrolled': enrolled.toString(),
+      'enrolled': enrolled, // Directly using the boolean value
     };
   }
 }
